dc5703aba08b78d78ab8b41435053ae182fc940f
Ali Attarwala <attrwl4@gmail.com>
2017-06-04 13:00:13 +0530

    build process

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var path = require('path');
+var gulp = require('gulp');
+var LessAutoprefix = require('less-plugin-autoprefix');
+var autoprefix = new LessAutoprefix({ browsers: ['last 4 versions', 'iOS>=7', 'android >= 4.0', 'and_uc >= 9.9'] });
+
+
+gulp.task('less', function () {
+  return gulp.src('./less/turmgel.less')
+    .pipe(less({
+      paths: [ path.join(__dirname, 'less', 'includes') ],
+      plugins: [autoprefix]
+    }))
+    .pipe(gulp.dest('./css'))
+});
